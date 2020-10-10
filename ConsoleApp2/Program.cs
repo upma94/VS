9284133be79089c7bd9eb7c16c869e7c33608a02
upma <upma@gmail.com>
2020-10-10 10:31:36 +0530

    first commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,9 +2,9 @@
 
 namespace ConsoleApp2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
         }
